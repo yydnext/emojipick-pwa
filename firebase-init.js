6442c212b08327c1b02ecd091c31829e3cf79f69
Young D. Yoo, Ph.D <yydnext@gmail.com>
2026-03-12 22:41:38 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -3,14 +3,23 @@
   if (window.__FIREBASE_READY__) return; // 중복 방지
 
   // ✅ 여기에 Firebase 콘솔의 config 그대로
-  const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "..."
-  };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVYc_K5ahGZ6TKpL4qUF0jXP16xtPOdww",
+  authDomain: "localboost-dev.firebaseapp.com",
+  projectId: "localboost-dev",
+  storageBucket: "localboost-dev.firebasestorage.app",
+  messagingSenderId: "262055028680",
+  appId: "1:262055028680:web:197f41affc87849d87c2fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
   firebase.initializeApp(firebaseConfig);
   window.db = firebase.firestore();
